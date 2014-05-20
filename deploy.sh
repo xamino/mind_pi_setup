68fb27bf49746dcfe8cb399b77c3e741041e97c5
Andreas <andreas.koell@uni-ulm.de>
2014-05-20 17:58:32 +0200

    run pi on ssid pi-mind

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+﻿#!/bin/sh
 
 echo "installing stuff"
 sudo apt-get update
-sudo apt-get install -qy iw tcpdump ifrename iceweasel matchbox-keyboard
+sudo apt-get install -qy iw tcpdump ifrename iceweasel hostapd
 echo "setup connection"
 sudo cp iftab /etc/iftab
 sudo cp interfaces /etc/network/interfaces
@@ -11,10 +11,14 @@ sudo cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 sudo cp checkconnection /usr/bin/checkconnection
 sudo cp checkconnectionstart /etc/init.d/checkconnectionstart
 sudo cp wlan-monitor /etc/init.d/wlan-monitor
+sudo cp hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp hostapd /etc/default/hostapd
 echo "now update-rd.de"
 sudo chmod +x /etc/init.d/wlan-monitor
 sudo chmod +x /etc/init.d/checkconnectionstart
-sudo update-rc.d wlan-monitor defaults
+sudo chmod +x /etc/init.d/hostapd
+#sudo update-rc.d wlan-monitor defaults
+sudo update-rc.d -f wlan-monitor remove
 sudo update-rc.d checkconnectionstart defaults
 
 #echo "setup display overscan"
@@ -22,7 +26,6 @@ sudo cp config.txt /boot/config.txt
 
 echo "setup touch input"
 sudo cp modules /etc/modules
-sudo cp keyboard.sh /home/pi/Desktop/keyboard.sh
 
 #copy link
 echo "install link"
--- keyboard.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-matchbox-keyboard
